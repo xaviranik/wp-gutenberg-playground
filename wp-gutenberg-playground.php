e8fbec44396c086ebaf52b61f326261785e4866b
Zabir Anik <xaviranik.me@gmail.com>
2022-07-18 02:00:40 +0600

    add: block json

--- wp-gutenberg-playground.php
+++ wp-gutenberg-playground.php
@@ -11,3 +11,9 @@
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain: wp-gp
  */
+
+ function wp_gp_init() {
+    register_block_type_from_metadata( __DIR__ );
+ }
+
+add_action( 'init', 'wp_gp_init' );
